bad581e6876c68e3e41f535ef632277c3dda3ae9
Bagus purnomo putra setiawan <bptra0510@gmail.com>
2025-02-24 09:29:14 +0700

    Add files via upload

---
+++ welcom.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h3>Selamat Belajar</h3>
+<?php
+    $_nama = "Ahmad Dzanuri";
+    $_umur = 20;
+    $_berat = 60.5;
+    $_prodi = "Sistem Informasi";
+    $_kampus = "STT Terpadu Nurul Fikri";
+    echo " Hello, Selamat datang di php"
+?>
+<hr>
+<?php
+    echo "Nama saya $_nama. Saya berumur $_umur tahun";
+    echo "<br>dengan berat badan $_berat kg";
+    echo "<br>saya sedang menempuh pendidikan di prodi $_prodi di kampus $_kampus";
+?>
+</body>
+</html>
